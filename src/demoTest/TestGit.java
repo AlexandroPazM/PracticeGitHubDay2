9413390c6f5e895153db02769a5e5bdf2875f02d
Latitude 7480 <Latitude 7480@ALEXANDRO.lan>
2023-10-05 17:56:21 -0400

    Commit 2

--- src/demoTest/TestGit.java
+++ src/demoTest/TestGit.java
@@ -1,5 +1,10 @@
 package demoTest;
 
 public class TestGit {
+	 public static void main(String[] args) {
+		
+		 System.out.println("G is the best......");
+		 System.out.println("I learnd Coding every day");
+	}
 
 }
